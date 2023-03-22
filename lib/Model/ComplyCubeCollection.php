1963498f42b9b9d54f5f381e7d4963e59b91317e
Paul <paul@brainspark.nl>
2023-03-22 10:23:56 +0100

    Add return type declaration to ComplyCubeCollection
    To prevent a deprecation warning in modern PHP 8.x
    
    Deprecated: Return type of ComplyCube\Model\ComplyCubeCollection::current() should either be compatible with Iterator::current(): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice
    Deprecated:  Return type of ComplyCube\Model\ComplyCubeCollection::next() should either be compatible with Iterator::next(): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice
    Deprecated:  Return type of ComplyCube\Model\ComplyCubeCollection::key() should either be compatible with Iterator::key(): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice
    Deprecated:  Return type of ComplyCube\Model\ComplyCubeCollection::valid() should either be compatible with Iterator::valid(): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice
    Deprecated:  Return type of ComplyCube\Model\ComplyCubeCollection::rewind() should either be compatible with Iterator::rewind(): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice

--- lib/Model/ComplyCubeCollection.php
+++ lib/Model/ComplyCubeCollection.php
@@ -24,27 +24,27 @@ class ComplyCubeCollection implements \Iterator
         $this->totalItems = $apiResponse->totalItems;
     }
 
-    public function rewind()
+    public function rewind(): void
     {
         $this->position = 0;
     }
 
-    public function current()
+    public function current(): mixed
     {
         return $this->items[$this->position];
     }
 
-    public function key()
+    public function key(): mixed
     {
         return $this->position;
     }
 
-    public function next()
+    public function next(): void
     {
         ++$this->position;
     }
 
-    public function valid()
+    public function valid(): bool
     {
         return isset($this->items[$this->position]);
     }
